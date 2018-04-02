3b6d417edda4cceddb8a43c72533383d3b8c466f
JamesFrowen-PC\JamesFrowen <jamesfrowendev@gmail.com>
2018-04-02 13:11:43 +0100

    intial code commit

---
+++ model-texture-base-color/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace model_texture_base_color
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.saveOutputDialog = new System.Windows.Forms.SaveFileDialog();
+            this.openOutlineDialog = new System.Windows.Forms.OpenFileDialog();
+            this.openTextureDialog = new System.Windows.Forms.OpenFileDialog();
+            this.openOutlineButton = new System.Windows.Forms.Button();
+            this.outlineBox = new System.Windows.Forms.PictureBox();
+            this.textureBox = new System.Windows.Forms.PictureBox();
+            this.openTextureButton = new System.Windows.Forms.Button();
+            this.resultBox = new System.Windows.Forms.PictureBox();
+            this.saveResultButton = new System.Windows.Forms.Button();
+            this.createResultButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.outlineBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // saveOutputDialog
+            // 
+            this.saveOutputDialog.FileName = "saveOutput";
+            // 
+            // openOutlineDialog
+            // 
+            this.openOutlineDialog.FileName = "openOutline";
+            // 
+            // openTextureDialog
+            // 
+            this.openTextureDialog.FileName = "openTexture";
+            // 
+            // openOutlineButton
+            // 
+            this.openOutlineButton.Location = new System.Drawing.Point(12, 12);
+            this.openOutlineButton.Name = "openOutlineButton";
+            this.openOutlineButton.Size = new System.Drawing.Size(256, 23);
+            this.openOutlineButton.TabIndex = 0;
+            this.openOutlineButton.Text = "Open Outline";
+            this.openOutlineButton.UseVisualStyleBackColor = true;
+            this.openOutlineButton.Click += new System.EventHandler(this.openOutlineButton_Click);
+            // 
+            // outlineBox
+            // 
+            this.outlineBox.Location = new System.Drawing.Point(12, 41);
+            this.outlineBox.Name = "outlineBox";
+            this.outlineBox.Size = new System.Drawing.Size(256, 256);
+            this.outlineBox.TabIndex = 1;
+            this.outlineBox.TabStop = false;
+            // 
+            // textureBox
+            // 
+            this.textureBox.Location = new System.Drawing.Point(12, 332);
+            this.textureBox.Name = "textureBox";
+            this.textureBox.Size = new System.Drawing.Size(256, 256);
+            this.textureBox.TabIndex = 3;
+            this.textureBox.TabStop = false;
+            // 
+            // openTextureButton
+            // 
+            this.openTextureButton.Location = new System.Drawing.Point(12, 303);
+            this.openTextureButton.Name = "openTextureButton";
+            this.openTextureButton.Size = new System.Drawing.Size(256, 23);
+            this.openTextureButton.TabIndex = 2;
+            this.openTextureButton.Text = "Open Texture";
+            this.openTextureButton.UseVisualStyleBackColor = true;
+            this.openTextureButton.Click += new System.EventHandler(this.openTextureButton_Click);
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(279, 59);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(512, 512);
+            this.resultBox.TabIndex = 5;
+            this.resultBox.TabStop = false;
+            // 
+            // saveResultButton
+            // 
+            this.saveResultButton.Location = new System.Drawing.Point(536, 12);
+            this.saveResultButton.Name = "saveResultButton";
+            this.saveResultButton.Size = new System.Drawing.Size(256, 23);
+            this.saveResultButton.TabIndex = 4;
+            this.saveResultButton.Text = "Save Result";
+            this.saveResultButton.UseVisualStyleBackColor = true;
+            this.saveResultButton.Click += new System.EventHandler(this.saveResultButton_Click);
+            // 
+            // createResultButton
+            // 
+            this.createResultButton.Location = new System.Drawing.Point(274, 12);
+            this.createResultButton.Name = "createResultButton";
+            this.createResultButton.Size = new System.Drawing.Size(256, 23);
+            this.createResultButton.TabIndex = 6;
+            this.createResultButton.Text = "Create Result";
+            this.createResultButton.UseVisualStyleBackColor = true;
+            this.createResultButton.Click += new System.EventHandler(this.createResultButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 604);
+            this.Controls.Add(this.createResultButton);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.saveResultButton);
+            this.Controls.Add(this.textureBox);
+            this.Controls.Add(this.openTextureButton);
+            this.Controls.Add(this.outlineBox);
+            this.Controls.Add(this.openOutlineButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.outlineBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveOutputDialog;
+        private System.Windows.Forms.OpenFileDialog openOutlineDialog;
+        private System.Windows.Forms.OpenFileDialog openTextureDialog;
+        private System.Windows.Forms.Button openOutlineButton;
+        private System.Windows.Forms.PictureBox outlineBox;
+        private System.Windows.Forms.PictureBox textureBox;
+        private System.Windows.Forms.Button openTextureButton;
+        private System.Windows.Forms.PictureBox resultBox;
+        private System.Windows.Forms.Button saveResultButton;
+        private System.Windows.Forms.Button createResultButton;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ model-texture-base-color/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace model_texture_base_color
+{
+    public partial class Form1 : Form
+    {
+        const byte OUTLINE_THRESHOLD = 100;
+        const string LOAD_FORMATS = "Image Files(*.PNG;*.BMP;*.JPG;*.GIF)|*.PNG;*.BMP;*.JPG;*.GIF|All files (*.*)|*.*";
+        const string SAVE_FORMATS = "PNG Image|*.png|JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+
+        readonly Image loading = Properties.Resources.LoadingImage;
+
+        Random rand = new Random();
+        Bitmap outLine;
+        Bitmap texture;
+        Bitmap result;
+
+        bool resultTaskFinished;
+
+        public Form1()
+        {
+            InitializeComponent();
+            setupDialogs(this.openOutlineDialog, "Open Outline Image");
+            setupDialogs(this.openTextureDialog, "Open Texture Image");
+            setupDialogs(this.saveOutputDialog, "Save Result Image");
+
+            setupPictureBox(this.outlineBox);
+            setupPictureBox(this.textureBox);
+            setupPictureBox(this.resultBox);
+
+            checkCreatebuttonEnabled();
+            checkSavebuttonEnabled();
+        }
+        private static void setupDialogs(FileDialog dialog, string title = "Select File")
+        {
+            if (dialog is OpenFileDialog open)
+            {
+                open.Multiselect = false;
+                dialog.Filter = LOAD_FORMATS;
+            }
+            else if (dialog is SaveFileDialog save)
+            {
+                dialog.Filter = SAVE_FORMATS;
+            }
+            dialog.Title = title;
+        }
+        private static void setupPictureBox(PictureBox box)
+        {
+            box.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void checkCreatebuttonEnabled()
+        {
+            this.createResultButton.Enabled = this.outLine != null && this.texture != null;
+        }
+        private void checkSavebuttonEnabled()
+        {
+            this.saveResultButton.Enabled = this.result != null;
+        }
+
+        private void openOutlineButton_Click(object sender, EventArgs e)
+        {
+            if (this.openOutlineDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.outLine = new Bitmap(this.openOutlineDialog.FileName);
+                this.outlineBox.Image = this.outLine;
+                checkCreatebuttonEnabled();
+            }
+        }
+
+        private void openTextureButton_Click(object sender, EventArgs e)
+        {
+            if (this.openTextureDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.texture = new Bitmap(this.openTextureDialog.FileName);
+                this.textureBox.Image = this.texture;
+                checkCreatebuttonEnabled();
+            }
+        }
+
+        private void createResultButton_Click(object sender, EventArgs e)
+        {
+            this.resultBox.Image = this.loading;
+
+            this.resultTaskFinished = false;
+            Task task = new Task(() =>
+            {
+                this.result = new Bitmap(this.outLine);
+
+                var width = this.result.Width;
+                var height = this.result.Height;
+                for (int x = 0; x < width; x++)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        var outLineColor = this.outLine.GetPixel(x, y);
+                        if (outLineColor.A > OUTLINE_THRESHOLD)
+                        {
+                            this.result.SetPixel(x, y, texturePixel(x, y));
+                        }
+                    }
+                }
+
+                this.resultBox.Image = this.result;
+                this.resultTaskFinished = true;
+            });
+            this.timer1.Start();
+            task.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.resultTaskFinished)
+            {
+                this.checkSavebuttonEnabled();
+                this.timer1.Stop();
+            }
+        }
+
+        private Color texturePixel(int xIn, int yIn)
+        {
+            //var x = xIn % this.texture.Width;
+            //var y = yIn % this.texture.Height;
+
+            // add tiled or other methods here?
+
+            return this.texture.GetPixel(
+                this.rand.Next(this.texture.Width),
+                this.rand.Next(this.texture.Height)
+            );
+        }
+
+        private void saveResultButton_Click(object sender, EventArgs e)
+        {
+            if (this.saveOutputDialog.ShowDialog() == DialogResult.OK)
+            {
+                var fileName = this.saveOutputDialog.FileName;
+
+                ImageFormat format;
+                switch (this.saveOutputDialog.FilterIndex)
+                {
+                    case 1:
+                    default:
+                        format = ImageFormat.Png;
+                        break;
+                    case 2:
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case 3:
+                        format = ImageFormat.Bmp;
+                        break;
+                    case 4:
+                        format = ImageFormat.Gif;
+                        break;
+
+                }
+
+                this.result.Save(fileName, format);
+            }
+        }
+    }
+}
